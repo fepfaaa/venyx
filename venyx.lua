6d2cd425e12635e8bf8c4f6eff3b394fb618f1e3
fepfaaa <93022799+fepfaaa@users.noreply.github.com>
2021-12-20 10:02:27 +0700

    Update venyx.lua

--- venyx.lua
+++ venyx.lua
@@ -18,9 +18,9 @@ local themes = {
 	Background = Color3.fromRGB(24, 24, 24), 
 	Glow = Color3.fromRGB(0, 0, 0), 
 	Accent = Color3.fromRGB(10, 10, 10), 
-	LightContrast = Color3.fromRGB(20, 20, 20), 
-	DarkContrast = Color3.fromRGB(14, 14, 14),  
-	TextColor = Color3.fromRGB(255, 255, 255)
+	LightContrast = Color3.fromRGB(34, 0, 30), 
+	DarkContrast = Color3.fromRGB(34, 0, 30),  
+	TextColor = Color3.fromRGB(34, 0, 30)
 }
 
 do
